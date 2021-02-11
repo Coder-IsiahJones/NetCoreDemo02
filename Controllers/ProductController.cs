e75299329f8ad452681477b8e1785376ebf4410d
Coder-IsiahJones <jones.isiah.blhs@gmail.com>
2021-02-10 20:33:21 -0500

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Bogus;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreDemo02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreDemo02.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<ProductModel> productList = new List<ProductModel>();
+
+            productList.Add(new ProductModel { Id = 1, Name = "Mouse Pad", Price = 5.99m, Description = "A square piece of plastic to make mousing easier." });
+            productList.Add(new ProductModel { Id = 2, Name = "Web Pad", Price = 6.99m, Description = "A square piece of plastic to make mousing easier." });
+            productList.Add(new ProductModel { Id = 3, Name = "Cam Pad", Price = 7.99m, Description = "A square piece of plastic to make mousing easier." });
+            productList.Add(new ProductModel { Id = 4, Name = "Toe Pad", Price = 8.99m, Description = "A square piece of plastic to make mousing easier." });
+
+            // Bogus Example (Fake Data)
+            for (int i = 0; i < 100; i++)
+            {
+                productList.Add(new Faker<ProductModel>()
+                    .RuleFor(p => p.Id, i + 5)
+                    .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+                    .RuleFor(p => p.Price, f => f.Random.Decimal(100))
+                    .RuleFor(p => p.Description, f => f.Rant.Review())
+                    );
+            }
+
+            return View(productList);
+        }
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreDemo02.Models
+{
+    public class ProductModel
+    {
+
+        [DisplayName("Id Number")]
+        public int Id { get; set; }
+
+        [DisplayName("Product")]
+        public string Name { get; set; }
+
+        [DisplayName("Price")]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [DisplayName("Description")]
+        public string Description { get; set; }
+    }
+}
